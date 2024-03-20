481f314093b01341f0a222a95f58ee15d4e2c2a9
Coxeyyy <artemcxy@gmail.com>
2024-03-20 22:33:10 +0300

    Добавил 1 конструктор

--- src/main/java/ru/coxey/diplom/model/Employee.java
+++ src/main/java/ru/coxey/diplom/model/Employee.java
@@ -1,9 +1,26 @@
 package ru.coxey.diplom.model;
 
+import ru.coxey.diplom.model.enums.Role;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("Employee")
 public class Employee extends Person {
+
+    public Employee() {
+    }
+
+    public Employee(String login, String password, Role role) {
+        super(login, password, role);
+    }
+
+    public Employee(String login, Role role) {
+        super(login, role);
+    }
+
+    public Employee(String login) {
+        super(login);
+    }
 }
